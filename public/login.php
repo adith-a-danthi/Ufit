d7535ea909043b463113b534eaa1ee25e9e60c8b
Adith A Danthi <adith.danthi@gmail.com>
2019-11-14 20:12:37 +0530

    added login UI

---
+++ public/login.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+    <title>Login</title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Gym</a>
+</nav>
+<br>
+<div class="card" style="width: 30%; border-radius: 25px; margin-left: auto; margin-right: auto">
+    <div class="card-body" >
+        <div class="form-group">
+            <h5>Login</h5> <br>
+            <input type="email" class="form-control" id="email_id" placeholder="Email id">
+            <br>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+            <br>
+            <a class="btn btn-outline-dark" role="button">Login</a>
+        </div>
+    </div>
+</div>
+</body>
