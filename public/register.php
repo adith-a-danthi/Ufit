9935ccbb70c2884c72cdfb3f55549f246838d2f2
Adith A Danthi <adith.danthi@gmail.com>
2019-11-23 18:17:02 +0530

    registration process

--- public/register.php
+++ public/register.php
@@ -22,14 +22,14 @@
             <input type="email" class="form-control" name="email_id" placeholder="Email id" required> <br>
             <input type="tel" class="form-control" name="phone" placeholder="Phone Number" required> <br>
             <input type="password" class="form-control" name="password" placeholder="Password" required> <br>
-            <input type="password" class="form-control" name="r-password" placeholder="Confirm Password" required>
+            <input type="password" class="form-control" name="r_password" placeholder="Confirm Password" required>
             <hr>
             <div class="form-row">
                 <div class="col">
                     <label for="dob" class="col col-form-label">DOB</label>
                 </div>
                 <div class="col-sm-10">
-                    <input type="date" class="form-control" id="dob" placeholder="dob" required style="width: auto">
+                    <input type="date" class="form-control" name="dob" placeholder="dob" required style="width: auto">
                 </div>
             </div>
             <br>
@@ -53,15 +53,20 @@
                 </div>
             </div>
             <br>
-            <input type="number" class="form-control" id="height" placeholder="Height (cm)" required> <br>
-            <input type="number" class="form-control" id="weight" placeholder="Weight (kgs)" required> <br>
-            <input type="text" class="form-control" id="goal" placeholder="Goal" required>
+            <input type="number" class="form-control" name="height" placeholder="Height (cm)" required> <br>
+            <input type="number" class="form-control" name="weight" placeholder="Weight (kgs)" required> <br>
+            <input type="text" class="form-control" name="goal" placeholder="Goal" required>
             <hr>
+            <div class="row">
+                <div class="col md-6">
+
+                </div>
+            </div>
             <div class="form-inline">
                 <label class="mr-sm-2" for="batch">Batch</label>
-                <select class="custom-select mr-sm-2" id="batch">
+                <select class="custom-select mr-sm-2" name="batch">
                     <option selected>Time</option>
-                    <option value="1">5:00</option>
+                    <option value="1">5:00show</option>
                     <option value="2">6:00</option>
                     <option value="3">7:00</option>
                     <option value="4">8:00</option>
